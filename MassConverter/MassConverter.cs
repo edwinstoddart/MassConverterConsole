5c49755b5ba7900f6f30a557455228ba8d49ec7f
Edwin DeJesus <edwinstoddart.student@gmail.com>
2022-01-02 14:59:47 -0500

    Added [MassConverter.cs] as a static class, and added several more TODOs to it.

---
+++ MassConverter/MassConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassConverter
+{
+    class MassConverter
+    {
+        // TODO: Create methods to convert different measurements of mass in metric
+            // TODO: Create separate methods for converting between: Microgram <-> Miligram <-> Gram <-> Kilogram <-> Metric Ton
+            // TODO: Create a control method to simplify user input by chaining multiple methods together to convert between one mass to a significantly larger/smaller one
+        // TODO: Create methods to convert different measurements of mass in imperial
+            // TODO: Create separate methods for converting between: Ounce <-> Pound <-> Stone <-> US Ton <-> Imperial Ton
+            // TODO: Create a control method to simplify user input by chaining multiple methods together to convert between one mass to a significantly larger/smaller one
+        // TODO: Create methods to convert different measurements of mass between metric and imperial
+        // TODO: Create a master method to freely convert between any two types of mass
+    }
+}
--- MassConverter/Program.cs
+++ MassConverter/Program.cs
@@ -6,7 +6,6 @@ namespace MassConverter
     {
         static void Main(string[] args)
         {
-            // TODO: Create a static class for dealing with Mass Conversions within the Imperial and Metric Systems
             // TODO: Create a text-based interface for using either sets of calculations
         }
     }
