3988792aa8c26956ee82735a3a2609d7cdcf71d0
Edwin DeJesus <edwinstoddart.student@gmail.com>
2022-01-02 15:17:23 -0500

    Completed metric mass conversion methods and added a method to do multiple conversions in a row (in both directions). Added some [Console.WriteLine()] statements to [Program.cs] to test the methods.

--- MassConverter/MassConverter.cs
+++ MassConverter/MassConverter.cs
@@ -8,9 +8,64 @@ namespace MassConverter
 {
     class MassConverter
     {
-        // TODO: Create methods to convert different measurements of mass in metric
-            // TODO: Create separate methods for converting between: Microgram <-> Miligram <-> Gram <-> Kilogram <-> Metric Ton
-            // TODO: Create a control method to simplify user input by chaining multiple methods together to convert between one mass to a significantly larger/smaller one
+        // ================================================== Metric Methods ==================================================
+        // Ascending Metric Conversion Methods:
+        public static double MicrogramToMiligram(double microgram) { return microgram / 1000; }
+        public static double MiligramToGram(double miligram) { return miligram / 1000; }
+        public static double GramToKilogram(double gram) { return gram / 1000; }
+        public static double KilogramToMetricTon(double kilogram) { return kilogram / 1000; }
+        // Descending Metric Conversion Methods:
+        public static double MetricTonToKilogram(double metricTon) { return metricTon * 1000; }
+        public static double KilogramToGram(double kilogram) { return kilogram * 1000; }
+        public static double GramToMiligram(double gram) { return gram * 1000; }
+        public static double MiligramToMicrogram(double miligram) { return miligram * 1000; }
+
+        // Main Metric Conversion Method
+        public static double MetricConversions(double mass, int startingType, int endingType)
+        {
+            if (startingType == endingType) { return mass; }
+            double convertedMass = mass;
+
+            if (startingType > endingType)
+            {
+                startingType = -startingType;
+                endingType = -endingType;
+            }
+
+            switch (startingType)
+            {
+
+                case 1:
+                    convertedMass = MicrogramToMiligram(convertedMass);
+                    if (endingType == 2) { break; } else { goto case 2; }
+                case 2:
+                    convertedMass = MiligramToGram(convertedMass);
+                    if (endingType == 3) { break; } else { goto case 3; }
+                case 3:
+                    convertedMass = GramToKilogram(convertedMass);
+                    if (endingType == 4) { break; } else { goto case 4; }
+                case 4:
+                    convertedMass = KilogramToMetricTon(convertedMass);
+                    break;
+
+                case -5:
+                    convertedMass = MetricTonToKilogram(convertedMass);
+                    if (endingType == -4) { break; } else { goto case -4; }
+                case -4:
+                    convertedMass = KilogramToGram(convertedMass);
+                    if (endingType == -3) { break; } else { goto case -3; }
+                case -3:
+                    convertedMass = GramToMiligram(convertedMass);
+                    if (endingType == -2) { break; } else { goto case -2; }
+                case -2:
+                    convertedMass = MiligramToMicrogram(convertedMass);
+                    break;
+            }
+
+            return convertedMass;
+        }
+
+        // ================================================== Imperial Methods ==================================================
         // TODO: Create methods to convert different measurements of mass in imperial
             // TODO: Create separate methods for converting between: Ounce <-> Pound <-> Stone <-> US Ton <-> Imperial Ton
             // TODO: Create a control method to simplify user input by chaining multiple methods together to convert between one mass to a significantly larger/smaller one
--- MassConverter/Program.cs
+++ MassConverter/Program.cs
@@ -7,6 +7,21 @@ namespace MassConverter
         static void Main(string[] args)
         {
             // TODO: Create a text-based interface for using either sets of calculations
+
+            // Test Statements:
+            double startingAmount = 1;
+            Console.WriteLine(startingAmount + " microgram is: " + MassConverter.MicrogramToMiligram(startingAmount) + " miligrams.");
+            Console.WriteLine(startingAmount + " miligram is: " + MassConverter.MiligramToGram(startingAmount) + " grams.");
+            Console.WriteLine(startingAmount + " gram is: " + MassConverter.GramToKilogram(startingAmount) + " kilograms.");
+            Console.WriteLine(startingAmount + " kilogram is: " + MassConverter.KilogramToMetricTon(startingAmount) + " metric tons.");
+
+            Console.WriteLine(startingAmount + " metric ton is: " + MassConverter.MetricTonToKilogram(startingAmount) + " kilograms.");
+            Console.WriteLine(startingAmount + " kilogram is: " + MassConverter.KilogramToGram(startingAmount) + " grams.");
+            Console.WriteLine(startingAmount + " gram is: " + MassConverter.GramToMiligram(startingAmount) + " miligrams.");
+            Console.WriteLine(startingAmount + " miligram is: " + MassConverter.MiligramToMicrogram(startingAmount) + " micrograms.");
+
+            Console.WriteLine(startingAmount + " microgram is: " + MassConverter.MetricConversions(startingAmount, 1, 5) + " metric tons.");
+            Console.WriteLine(startingAmount + " metric ton is: " + MassConverter.MetricConversions(startingAmount, 5, 1) + " micrograms.");
         }
     }
 }
