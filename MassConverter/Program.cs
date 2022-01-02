8073a735d2e15878bc42797770d3d091c9f65fc6
Edwin DeJesus <edwinstoddart.student@gmail.com>
2022-01-02 14:43:11 -0500

    Added a list of TODOs to [Program.cs].

--- MassConverter/Program.cs
+++ MassConverter/Program.cs
@@ -6,7 +6,8 @@ namespace MassConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO: Create a static class for dealing with Mass Conversions within the Imperial and Metric Systems
+            // TODO: Create a text-based interface for using either sets of calculations
         }
     }
 }
